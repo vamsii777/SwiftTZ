bb11d6739644b55a52aeda32b5efe115d8cd2e19
vamsii777 <vamsi@dewonderstruck.com>
2024-11-22 01:59:33 +0530

    Refactor timezone parsing to support both now and historical zones

--- Package.swift
+++ Package.swift
@@ -1,47 +1,39 @@
-import CompilerPluginSupport
 // swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
-  name: "SwiftTZ",
-  platforms: [
-    .macOS(.v13),
-    .iOS(.v16),
-    .watchOS(.v9),
-    .tvOS(.v16),
-    .macCatalyst(.v16),
-    .visionOS(.v1),
-  ],
-  products: [
-    .library(name: "SwiftTZ", targets: ["SwiftTZ"]),
-    .executable(name: "tzdb-gen", targets: ["SwiftTZGenerator"]),
-  ],
-  dependencies: [
-    .package(url: "https://github.com/apple/pkl-swift.git", from: "0.3.0")
-  ],
-  targets: [
-    // Generator executable
-    .executableTarget(
-      name: "SwiftTZGenerator",
-      dependencies: [
-        .product(name: "PklSwift", package: "pkl-swift")
-      ]
-    ),
-
-    // Main library
-    .target(
-      name: "SwiftTZ",
-      dependencies: [
-        .product(name: "PklSwift", package: "pkl-swift")
-      ]
-    ),
-
-    // Tests
-    .testTarget(
-      name: "SwiftTZTests",
-      dependencies: [
-        "SwiftTZ"
-      ]
-    ),
-  ]
+    name: "swifttz",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v16),
+        .watchOS(.v9),
+        .tvOS(.v16),
+        .macCatalyst(.v16),
+        .visionOS(.v1)
+    ],
+    products: [
+        .library(name: "SwiftTZ", targets: ["SwiftTZ"]),
+        .executable(name: "tzdb-gen", targets: ["SwiftTZGenerator"]),
+    ],
+    targets: [
+        // Generator executable
+        .executableTarget(
+            name: "SwiftTZGenerator",
+            dependencies: []
+        ),
+        
+        // Main library
+        .target(
+            name: "SwiftTZ",
+            dependencies: []
+        ),
+        
+        // Tests
+        .testTarget(
+            name: "SwiftTZTests",
+            dependencies: [
+                "SwiftTZ"
+            ]
+        ),
+    ]
 )
